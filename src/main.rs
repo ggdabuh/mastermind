f68b9e88fb1cb8fdee09e841920d1c1a22d11cdc
Guillaume Guigue <ggdabuh@yahoo.fr>
2022-10-06 13:49:45 +0200

    clean

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::io;
 use std::cmp;
 use rayon::prelude::*;
-use std::sync::{Arc, Mutex};
 
 const RADIX: u32 = 10;
-
 
 type Row = Vec<u32>;
 
@@ -74,38 +72,7 @@ fn calc_min_eliminated(rows: & Vec<Row>, row: &Row, value_count: u32) -> u32 {
     return *res.iter().min().unwrap();
 }
 
-#[allow(dead_code)]
 fn best(rows: & Vec<Row>, value_count: u32) -> &Row {
-    let best = rows.iter().map(|row| {
-        let n = calc_min_eliminated(rows, &row, value_count);
-        return (n, row);
-    }).max_by(|x, y| {
-        return x.0.cmp(&y.0); 
-    });
-    return best.unwrap().1;
-}
-
-#[allow(dead_code)]
-fn best2(rows: & Vec<Row>, value_count: u32) -> &Row {
-
-    let best: (u32, Option<&Row>) = (0, None);
-    let data = Arc::new(Mutex::new(best));
-    rows.par_iter().for_each(|row| {
-         let n = calc_min_eliminated(rows, &row, value_count);
-         let mut data = data.lock().unwrap();
-         let (n2, _) = *data;
-         if n > n2 {
-             *data = (n, Some(&row));
-         }
-     });
-     let data = data.lock().unwrap();
-     let (_, row) = *data;
-     return row.unwrap();
-}
- 
-
-#[allow(dead_code)]
-fn best3(rows: & Vec<Row>, value_count: u32) -> &Row {
      let best = rows.par_iter().map(|row| {
          let n = calc_min_eliminated(rows, &row, value_count);
          return (n, row);
@@ -131,11 +98,10 @@ fn main() {
         .read_line(&mut line)
         .expect("Failed to read line");
 
-
     let value_count: u32 = line.trim().parse().expect("Please type a number!");
 
     let mut rows = init_rows(row_size, value_count);
-    let mut row = best3(&rows, value_count).clone();
+    let mut row = best(&rows, value_count).clone();
     loop {
         println!("{:?}", row);
         line.clear();
@@ -152,7 +118,7 @@ fn main() {
             row = rows[0].clone();
             break;
         }
-        row = best3(&rows, value_count).clone();
+        row = best(&rows, value_count).clone();
     }
     println!("{:?}", row);
 }
